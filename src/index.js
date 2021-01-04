83954601b9ad773e5128046d6e235c8094013d99
Artūrs Jānis Ņikitins <ajnikitins@gmail.com>
2021-01-05 00:10:31 +0200

    Add bootstrap and fontawesome icon dependency

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.css'
+
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
